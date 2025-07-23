4edbd5bedbed697e27055997bb67a01dd84aecc1
Milan-C-I <milancheriyamanep@gmail.com>
2025-07-23 22:43:04 +0530

    underconstruction for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      {/* <UnderConstructionBanner /> */}
+      <UnderConstructionBanner />
       <Portfolio />
     </>
   );
