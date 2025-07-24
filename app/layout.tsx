4bc98a490fbce2ccce91e0445145264122062c9d
Milan-C-I <milancheriyamanep@gmail.com>
2025-07-25 00:44:53 +0530

    additional modification of the theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="scroll-smooth">
+        {[...Array(20)].map((_, i) => (
+          <div
+            key={i}
+            className="fixed w-2 h-2 bg-red-600/30 rounded-full animate-twinkle floating"
+            style={{
+              left: `${Math.random() * 100}%`,
+              top: `${Math.random() * 100}%`,
+              animationDelay: `${Math.random() * 3}s`,
+              animationDuration: `${2 + Math.random() * 2}s`
+            }}
+          ></div>
+        ))}
+        <div className="fixed -top-30 -left-30   md:top-30 md:left-20 w-72 h-72 bg-orange-500/80 rounded-full blur-3xl animate-pulse"></div>
+        <div className="fixed inset-0 -z-10 animated-gradient" style={{ opacity: 1 }} />
         {children}
       </body>
     </html>
